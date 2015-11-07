f1016a0c562f693188e3962a7e386bf05c6412ca
Joel Carlbark <joel.carlbark@meltwater.com>
2015-11-07 01:47:36 +0100

    Add more logging in fetcher and email controller

--- lib/mallery/work/fetcher.ex
+++ lib/mallery/work/fetcher.ex
@@ -8,12 +8,14 @@ defmodule Mallery.Work.Fetcher do
 
   def fetch(%{url: url, dir: dir, name: name}, uploader) do
     target_file = "#{dir}/#{name}"
-    Logger.debug("Fetching #{inspect(%{url: url, to: target_file})}")
+    Logger.info("Fetching #{inspect(%{url: url, to: target_file})}")
 
     opts = [basic_auth: {"api", "key-c0e297ad36c4892d6fe93c19c41844a0"}]
     %HTTPoison.Response{body: data, status_code: 200} = HTTPoison.get!(url, [], [hackney: opts])
     
     File.write!(target_file, data)
+
+    Logger.info("Done with fetching #{target_file}")
     uploader.cast(:upload_pool, {:upload, [name]})
   end
 end
--- lib/mallery/work/uploader.ex
+++ lib/mallery/work/uploader.ex
@@ -1,11 +1,13 @@
 defmodule Mallery.Work.Uploader do
   use Honeydew
+  require Logger
 
   def init(state) do
     {:ok, state}
   end
 
   def upload(name, state) do
+    Logger.info("Starting to upload: #{name} to ...")
     :ok
   end
 end
